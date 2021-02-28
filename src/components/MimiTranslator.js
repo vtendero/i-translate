484ea731d84b172360e0d5e94f0d63454e606ec7
vtendero <tendero.valle@gmail.com>
2021-02-28 19:09:03 +0100

    add gif

--- src/components/MimiTranslator.js
+++ src/components/MimiTranslator.js
@@ -2,12 +2,19 @@ import '../styles/mimiTranslator.scss';
 import React, {useState} from 'react';
 
 const MimiTranslator = (props) => {
+    const [gif, setGif] = useState('');
+
+    const change
+
+    const hiddenClassName = 'hidden' ? 'hidden' : '';
+
   return (
         <section className='translator'>
             <div className='div'>
                 <h5 className='translator__title'>Traducción:</h5>
                 <p className='translator__text'>{props.text.replace(/[aeiouáéíóú]/gi, 'i')}</p>
             </div>
+            <img className={`translator__gif ${hiddenClassName}`} src='https://media.tenor.com/images/8810efc9d438ccc5c3c44bbf7c6455c9/tenor.gif' alt="Gif mimimi"></img>
         </section>
   );
     
